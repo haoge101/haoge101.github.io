a94e6accce282097e2e12110214d28b3c2d31de7
haoge101 <66171250+haoge101@users.noreply.github.com>
2022-03-25 14:28:33 +0800

    Update requestsAPI.py

--- requestsAPI.py
+++ requestsAPI.py
@@ -2,7 +2,7 @@
 
 import requests,json
 
-url = 'http://open.iciba.com/dsapi/'   # 目标网站，？wd=后面为参数，可以使用params，不过看起来不舒服
+url = 'http://open.iciba.com/dsapi/?date=2020-05-03'   # 目标网站，？wd=后面为参数，可以使用params，不过看起来不舒服
 r = requests.get(url)    # 发送请求，将准备好的url与headers参数放入
 r = json.loads(r.text)    # 返回url地址
 r1 = r['picture4']
