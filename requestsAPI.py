234a69bc1412586ff702980d467dc75b97d5392d
haoge101 <66171250+haoge101@users.noreply.github.com>
2022-03-28 18:35:19 +0800

    Update requestsAPI.py

--- requestsAPI.py
+++ requestsAPI.py
@@ -11,43 +11,41 @@ open('./img.jpg', 'wb').write(r2.content)
 r3 = r['content']
 
 str="""
-$(function(){
-        const text = document.querySelector('.texttype');
+const text = document.querySelector('.texttype');
 """
 t="""
-        const txt  =["HI, THERE!", "%s"];   
+const txt  =["HI, THERE!", "%s"];   
 """%(r3)		
 str2="""		
-        var index=0;
-        var xiaBiao= 0;
-        var huan = true;
-     
-        setInterval(function(){
-           
-            if(huan){      
-                text.innerHTML = txt[xiaBiao].slice(0,++index);
-                console.log(index);
-            }
-            else{
-                text.innerHTML = txt[xiaBiao].slice(0,index--);
-                console.log(index);
-            }
-            if(index==txt[xiaBiao].length+3)
-            {
-                huan = false;
-            }
-            else if(index<0)
-            {
-                index = 0;
-                huan = true;
-                xiaBiao++;
-                if(xiaBiao>=txt.length)
-                {
-                    xiaBiao=0; 
-                }
-            }
-        },150)
-});
+var index=0;
+var xiaBiao= 0;
+var huan = true;
+
+setInterval(function(){
+
+    if(huan){      
+        text.innerHTML = txt[xiaBiao].slice(0,++index);
+        console.log(index);
+    }
+    else{
+        text.innerHTML = txt[xiaBiao].slice(0,index--);
+        console.log(index);
+    }
+    if(index==txt[xiaBiao].length+3)
+    {
+        huan = false;
+    }
+    else if(index<0)
+    {
+        index = 0;
+        huan = true;
+        xiaBiao++;
+        if(xiaBiao>=txt.length)
+        {
+            xiaBiao=0; 
+        }
+    }
+},150)
 """
 
 f = open('roll.js', 'w')
