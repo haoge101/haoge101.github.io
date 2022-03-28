b6eefae54c9a30e3ff072c9c5c2b0b767f5b2f46
haoge101 <haoge101@163.com>
2022-03-28 10:18:06 +0000

    upload img.jpg

---
+++ roll.js
@@ -0,0 +1,36 @@
+
+<script>
+        const text = document.querySelector('.texttype');
+
+        const txt  =["HI, THERE!", "They say the important thing in life isn't the destination. It's the journey."];   
+		
+        var index=0;
+        var xiaBiao= 0;
+        var huan = true;
+     
+        setInterval(function(){
+           
+            if(huan){      
+                text.innerHTML = txt[xiaBiao].slice(0,++index);
+                console.log(index);
+            }
+            else{
+                text.innerHTML = txt[xiaBiao].slice(0,index--);
+                console.log(index);
+            }
+            if(index==txt[xiaBiao].length+3)
+            {
+                huan = false;
+            }
+            else if(index<0)
+            {
+                index = 0;
+                huan = true;
+                xiaBiao++;
+                if(xiaBiao>=txt.length)
+                {
+                    xiaBiao=0; 
+                }
+            }
+        },150)
+</script>
