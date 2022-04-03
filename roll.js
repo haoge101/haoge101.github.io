a4d8b7a17314f09ce0d76f7a51f51a82c67b8648
haoge101 <haoge101@163.com>
2022-04-03 11:09:53 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "When things are looking down, look up."];   
+const txt  =["HI, THERE!", "Part of life is learning to let things go."];   
 		
 var index=0;
 var xiaBiao= 0;
