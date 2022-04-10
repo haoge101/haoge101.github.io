e80872409b4e2e13258493ba806e914c49cfdfe5
haoge101 <haoge101@163.com>
2022-04-10 01:06:14 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Never underestimate your power to change yourself!"];   
+const txt  =["HI, THERE!", "Nothing for nothing."];   
 		
 var index=0;
 var xiaBiao= 0;
