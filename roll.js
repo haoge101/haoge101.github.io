88924a05047fae8fbe94b044b55363a5a18a5d30
haoge101 <haoge101@163.com>
2022-04-18 01:16:49 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "He who laughs last laughs best. "];   
+const txt  =["HI, THERE!", "Perseverance can make miracles happen. "];   
 		
 var index=0;
 var xiaBiao= 0;
