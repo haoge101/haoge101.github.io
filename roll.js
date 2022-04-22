2b1c9fa985cc3f70c1c54f80297f8f96cc01cbf9
haoge101 <haoge101@163.com>
2022-04-22 01:14:56 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Pursue breakthroughs in your life. "];   
+const txt  =["HI, THERE!", "Everyday and in every way I'm getting better. "];   
 		
 var index=0;
 var xiaBiao= 0;
