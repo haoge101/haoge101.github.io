27f4bfd892cff704ea7acb80dd638e2d20088cd6
haoge101 <haoge101@163.com>
2022-04-24 01:05:02 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Book and friends should be few but good. "];   
+const txt  =["HI, THERE!", "All things come to those who wait."];   
 		
 var index=0;
 var xiaBiao= 0;
