6b03737e7e133b84f89f1af822ac61ad4fc2dfb4
haoge101 <haoge101@163.com>
2022-04-25 01:07:32 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "All things come to those who wait."];   
+const txt  =["HI, THERE!", "A fool will fool himself, but time will not fool anyone."];   
 		
 var index=0;
 var xiaBiao= 0;
