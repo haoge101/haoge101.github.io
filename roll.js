818e59504fed22549451fdd6e280b65f3c88326a
haoge101 <haoge101@163.com>
2022-04-30 01:22:25 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Compromise where you can. Where you can't, don't."];   
+const txt  =["HI, THERE!", "Winner makes the rules, and losers live by them."];   
 		
 var index=0;
 var xiaBiao= 0;
