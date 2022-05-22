6128ebdb8b08410a2438423feabeac50a519836b
haoge101 <haoge101@163.com>
2022-05-22 01:11:37 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "There's no rhyme or reason to this life."];   
+const txt  =["HI, THERE!", "It is much more difficult to judge oneself than to judge others."];   
 		
 var index=0;
 var xiaBiao= 0;
