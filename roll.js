33f76f755d3a2a2ddfd8c85c0ac52ebfc001fe5c
haoge101 <haoge101@163.com>
2022-06-12 01:26:20 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Eternity is not a distance but a decision."];   
+const txt  =["HI, THERE!", "To sin by silence when we should protest makes cowards out of men."];   
 		
 var index=0;
 var xiaBiao= 0;
