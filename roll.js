166b072a433618d92c3f66e0cd45d01fb01e163e
haoge101 <haoge101@163.com>
2022-06-14 01:27:35 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Miracles sometimes occur, but one has to work terribly for them."];   
+const txt  =["HI, THERE!", "Great things never came from comfort zones."];   
 		
 var index=0;
 var xiaBiao= 0;
