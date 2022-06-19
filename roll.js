4b6abb4e8c60395c785a51a5d8109e8cd27fe198
haoge101 <haoge101@163.com>
2022-06-19 01:20:48 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Loving you is the most grand and joyful secret in my youth."];   
+const txt  =["HI, THERE!", "The first time I hugged the sky was when you held me above your head. "];   
 		
 var index=0;
 var xiaBiao= 0;
