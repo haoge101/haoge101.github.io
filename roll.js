ed10b06eaa2007e7b951fa9b133c4fca881ce55e
haoge101 <haoge101@163.com>
2022-06-27 01:26:28 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Those who follow the light will eventually be radiant."];   
+const txt  =["HI, THERE!", "If you think the road is difficult, it must be uphill."];   
 		
 var index=0;
 var xiaBiao= 0;
