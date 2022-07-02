73ad626d210b214d16077473d55bebc1231d155f
haoge101 <haoge101@163.com>
2022-07-02 01:18:20 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Without a great party, there will be no prosperity and well-being."];   
+const txt  =["HI, THERE!", "Self-discipline is the first condition of freedom."];   
 		
 var index=0;
 var xiaBiao= 0;
