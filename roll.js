a0a0de9f6a951cd9e7fd9132849e673423aec5c0
haoge101 <haoge101@163.com>
2022-07-13 01:21:40 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Love rises in the east and falls in the west, romance lasts till death."];   
+const txt  =["HI, THERE!", "It's futile to rectify the past, but you can still change the future."];   
 		
 var index=0;
 var xiaBiao= 0;
