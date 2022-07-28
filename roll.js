761b27d19195b9dd7414c299f54b63a7c884bea7
haoge101 <haoge101@163.com>
2022-07-28 01:24:20 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Stay hungry, stay foolish."];   
+const txt  =["HI, THERE!", "I am still waiting for you in situ, but you have forgotten you had been here once."];   
 		
 var index=0;
 var xiaBiao= 0;
