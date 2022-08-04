b470446b260ec8c2cebed81d1d8c6d8103ca62b2
haoge101 <haoge101@163.com>
2022-08-04 01:34:57 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Shoot for the moon; even if you miss, you'll land among the stars."];   
+const txt  =["HI, THERE!", "The deepest and heaviest love must grow up with the time."];   
 		
 var index=0;
 var xiaBiao= 0;
