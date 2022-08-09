9058ae5b75ca76bb1de489e75ca6e591336de869
haoge101 <haoge101@163.com>
2022-08-09 01:40:32 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "To lost in something you love is better than to win in something you hate."];   
+const txt  =["HI, THERE!", "Many people will walk in and out of your life, but only true friends will leave footprints in your heart."];   
 		
 var index=0;
 var xiaBiao= 0;
