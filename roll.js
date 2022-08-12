360a0993bdf906fb97fd0b82c56944d18424e1a4
haoge101 <haoge101@163.com>
2022-08-12 01:20:21 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "The truth is a beautiful and terrible thing, and should therefore be treated with great caution."];   
+const txt  =["HI, THERE!", "Death is not the end of life, forgetting is."];   
 		
 var index=0;
 var xiaBiao= 0;
