66555242d13123a437c6c0f9c92498f180342e08
haoge101 <haoge101@163.com>
2022-08-25 01:42:35 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Don't just sit at home, or the life of your dreams will sail right past you."];   
+const txt  =["HI, THERE!", "They who cannot do as they would, must do as they can."];   
 		
 var index=0;
 var xiaBiao= 0;
