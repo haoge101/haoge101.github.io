bafbb506e010ae1f2c3b7f5f3eada9d83d4b619d
haoge101 <haoge101@163.com>
2022-08-30 01:55:36 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Never compromise. Not even in the face of armageddon."];   
+const txt  =["HI, THERE!", "A little of every thing is nothing in the main."];   
 		
 var index=0;
 var xiaBiao= 0;
