b544d3d1b4d381ccc730889e2b781e976da88c71
haoge101 <haoge101@163.com>
2022-09-04 01:40:28 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Let's cross the bridge when we come to it."];   
+const txt  =["HI, THERE!", " The beauty of summer can heal the winter of your life."];   
 		
 var index=0;
 var xiaBiao= 0;
