bc3528bd4c7d4f39bb3ade1ec23a3e7aef2f1f03
haoge101 <haoge101@163.com>
2022-10-31 01:48:43 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "True masters tend to focus on one thing and do it well."];   
+const txt  =["HI, THERE!", "Kill the evil out!"];   
 		
 var index=0;
 var xiaBiao= 0;
