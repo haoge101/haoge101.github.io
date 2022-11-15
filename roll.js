3319bf4576c63c887907f0ee58abac9a0670fd5e
haoge101 <haoge101@163.com>
2022-11-15 01:17:50 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Miracles happen every day."];   
+const txt  =["HI, THERE!", "Words and ideas can change the world."];   
 		
 var index=0;
 var xiaBiao= 0;
