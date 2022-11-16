174d8dde15b5fcadda87259f7452d7a37913a7ec
haoge101 <haoge101@163.com>
2022-11-16 01:18:03 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Words and ideas can change the world."];   
+const txt  =["HI, THERE!", "After all, tomorrow is another day."];   
 		
 var index=0;
 var xiaBiao= 0;
