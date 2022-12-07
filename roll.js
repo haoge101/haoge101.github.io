ab0ded8eaf119bc28c538e14eb81ad5f56c541eb
haoge101 <haoge101@163.com>
2022-12-07 01:08:08 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "The deepest and heaviest love must grow up with the time."];   
+const txt  =["HI, THERE!", "A timely snow promises a good harvest."];   
 		
 var index=0;
 var xiaBiao= 0;
