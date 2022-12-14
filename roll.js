4ff7ad21d87e6e57f798c2b816cbcc6a8cf90e2f
haoge101 <haoge101@163.com>
2022-12-14 01:06:49 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Witnesses pass, but history is eternal. - From China Daily"];   
+const txt  =["HI, THERE!", "No victory comes without a price."];   
 		
 var index=0;
 var xiaBiao= 0;
