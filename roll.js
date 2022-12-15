9dad4226fbafc39a710bbaa819b973c393eac721
haoge101 <haoge101@163.com>
2022-12-15 01:08:24 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "No victory comes without a price."];   
+const txt  =["HI, THERE!", "Be good. Be bad. Just be yourself."];   
 		
 var index=0;
 var xiaBiao= 0;
