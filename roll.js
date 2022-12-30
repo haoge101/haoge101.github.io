bab4f271c7fcf26a8d36e3ff06881957e557ff49
haoge101 <haoge101@163.com>
2022-12-30 01:04:28 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Human beings are designed for many things, but loneliness isn't one of them."];   
+const txt  =["HI, THERE!", "Everybody deserves to be loved."];   
 		
 var index=0;
 var xiaBiao= 0;
