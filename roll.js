92ef87b165a88ce8913837989e3737b65f7a1bcf
haoge101 <haoge101@163.com>
2023-01-03 01:03:11 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "We all carry something with us."];   
+const txt  =["HI, THERE!", "The only way to achieve the impossible is to believe it is possible."];   
 		
 var index=0;
 var xiaBiao= 0;
