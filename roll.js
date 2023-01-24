f3237de64ee667a0c57204d0fcc4392767ef997f
haoge101 <haoge101@163.com>
2023-01-24 01:07:54 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Your faith is all that keeps me going."];   
+const txt  =["HI, THERE!", "A good laugh and a long sleep are the best cures in the doctor's book."];   
 		
 var index=0;
 var xiaBiao= 0;
