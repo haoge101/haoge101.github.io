39f27fc7598d7f42fdb7c5249e19a7907964e5fd
haoge101 <haoge101@163.com>
2023-01-27 01:08:00 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Spring cleaning can't wait!"];   
+const txt  =["HI, THERE!", "With every sun comes a new day."];   
 		
 var index=0;
 var xiaBiao= 0;
