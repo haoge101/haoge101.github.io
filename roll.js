55d5335ee67260130f32daa7e93add89cc76e73e
haoge101 <haoge101@163.com>
2023-01-30 01:02:28 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "A smile is the most charming part of a person forever."];   
+const txt  =["HI, THERE!", "Let's take a long break from work."];   
 		
 var index=0;
 var xiaBiao= 0;
