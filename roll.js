c7b06f964692de9c9a5b47f20e014d96010b918d
haoge101 <haoge101@163.com>
2023-02-03 01:09:20 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "May life be quiet and sweet every year."];   
+const txt  =["HI, THERE!", "The glass is full, and the sound is joyful."];   
 		
 var index=0;
 var xiaBiao= 0;
