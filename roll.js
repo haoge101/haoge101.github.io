1971fce98cbb5e490fd3968a4f84d7dba12c2eca
haoge101 <haoge101@163.com>
2023-02-07 01:05:53 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Could I have some extra days off?"];   
+const txt  =["HI, THERE!", "If you are hopeful, you will be invincible."];   
 		
 var index=0;
 var xiaBiao= 0;
