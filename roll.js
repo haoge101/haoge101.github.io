047dc171930b92b1743fdb1ffe02e5a8413f1565
haoge101 <haoge101@163.com>
2023-02-08 01:07:27 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "If you are hopeful, you will be invincible."];   
+const txt  =["HI, THERE!", "Treasured memories live and grow more precious with time."];   
 		
 var index=0;
 var xiaBiao= 0;
