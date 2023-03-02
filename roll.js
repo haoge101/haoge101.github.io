34486d39074ed28584bf45897628fc026cb3563b
haoge101 <haoge101@163.com>
2023-03-02 01:14:04 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Failure is the fog through which we glimpse triumph."];   
+const txt  =["HI, THERE!", "Every obstacle is an opportunity."];   
 		
 var index=0;
 var xiaBiao= 0;
