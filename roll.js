73cc2785ed2c95a4829fcf199ec81a5851423976
haoge101 <haoge101@163.com>
2023-03-06 01:09:32 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Human life is limited, and serving the people is infinite."];   
+const txt  =["HI, THERE!", "People always prepare for the worst but hope for the best."];   
 		
 var index=0;
 var xiaBiao= 0;
