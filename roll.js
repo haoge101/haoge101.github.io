7d9eb6ec038a16c2e7d2fa1855692f7639b1c336
haoge101 <haoge101@163.com>
2023-03-31 01:05:06 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Medicines cure diseases but only doctors can cure patients."];   
+const txt  =["HI, THERE!", "Compromise where you can. Where you can't, don't."];   
 		
 var index=0;
 var xiaBiao= 0;
