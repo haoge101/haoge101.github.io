cca0dfe06940245f5052772a42d64f5d563ab63e
haoge101 <haoge101@163.com>
2023-04-02 01:05:10 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "The best state of life is to be stupid."];   
+const txt  =["HI, THERE!", "Youth means limitless possibilities. "];   
 		
 var index=0;
 var xiaBiao= 0;
