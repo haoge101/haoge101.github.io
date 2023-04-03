df7ffd9f2ec371ca13126d90199e7b3467557daa
haoge101 <haoge101@163.com>
2023-04-03 01:00:11 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Youth means limitless possibilities. "];   
+const txt  =["HI, THERE!", "Part of life is learning to let things go."];   
 		
 var index=0;
 var xiaBiao= 0;
