b386bd91250b8ff9bb5a45a747b17db4a78d8029
haoge101 <haoge101@163.com>
2023-05-06 00:57:23 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "I love summer because it is full of life and hope. "];   
+const txt  =["HI, THERE!", "No water, no life."];   
 		
 var index=0;
 var xiaBiao= 0;
