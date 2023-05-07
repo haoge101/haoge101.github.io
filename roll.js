e54669735f6b0ff813b850115bc8582175b54284
haoge101 <haoge101@163.com>
2023-05-07 01:06:01 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "No water, no life."];   
+const txt  =["HI, THERE!", "The hard thing to do and the right thing to do are usually the same thing. "];   
 		
 var index=0;
 var xiaBiao= 0;
