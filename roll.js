ab111906b6c3f5d61c049e1bc30c64ab389a2286
haoge101 <haoge101@163.com>
2023-05-19 01:03:00 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "No road of flowers leads to glory."];   
+const txt  =["HI, THERE!", "It's okay to be scared. In order to be brave, we got to be a little scared."];   
 		
 var index=0;
 var xiaBiao= 0;
