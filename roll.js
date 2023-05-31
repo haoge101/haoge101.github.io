68808e9c7450782b5984000d4602da1922b4c1a0
haoge101 <haoge101@163.com>
2023-05-31 01:11:18 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Food is worth more than gold."];   
+const txt  =["HI, THERE!", "Every effort is the foreshadowing of luck."];   
 		
 var index=0;
 var xiaBiao= 0;
