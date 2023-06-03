005c8bcdd1ec7ea9776aa9d6bddc307d8a2b7f9b
haoge101 <haoge101@163.com>
2023-06-03 01:10:48 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Every effort is the foreshadowing of luck."];   
+const txt  =["HI, THERE!", "The darker the sky, the brighter the stars. "];   
 		
 var index=0;
 var xiaBiao= 0;
