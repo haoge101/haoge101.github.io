1ac06b51c5ec8def54a7125e8ef5144b83886e3e
haoge101 <haoge101@163.com>
2023-06-07 01:14:39 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "All your efforts will pay off one day."];   
+const txt  =["HI, THERE!", "Turn sweat into pearls and dreams into reality."];   
 		
 var index=0;
 var xiaBiao= 0;
