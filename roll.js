bf8e576ad92f84d8f1520253c176946127efa129
haoge101 <haoge101@163.com>
2023-06-10 01:07:30 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "We are all in the gutter, but some of us are looking at the stars."];   
+const txt  =["HI, THERE!", "Youth is not afraid of the long years, there is still glory in the other side."];   
 		
 var index=0;
 var xiaBiao= 0;
