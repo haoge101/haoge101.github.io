dec4341c67b2711f3b0dc6ed6a4bc93869fafd5f
haoge101 <haoge101@163.com>
2023-06-14 01:08:41 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Miracles sometimes occur, but one has to work terribly for them."];   
+const txt  =["HI, THERE!", "Our greatest glory consists not in never falling, but in rising every time."];   
 		
 var index=0;
 var xiaBiao= 0;
