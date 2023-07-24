78a44632f752fcd1cf04c6c4f2fc273eec319dc0
haoge101 <haoge101@163.com>
2023-07-24 01:08:21 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Love is imagination. The other is the carrier. It's you who give the meaning to it."];   
+const txt  =["HI, THERE!", "Shoot for the moon; even if you miss, you'll land among the stars."];   
 		
 var index=0;
 var xiaBiao= 0;
