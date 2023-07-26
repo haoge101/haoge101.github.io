9baf1244d26790581bc93fa97b213834613cd737
haoge101 <haoge101@163.com>
2023-07-26 01:07:00 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "The smartest people understand that they are the most ignorant ones. "];   
+const txt  =["HI, THERE!", "A little of every thing is nothing in the main."];   
 		
 var index=0;
 var xiaBiao= 0;
