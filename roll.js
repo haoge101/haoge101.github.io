6dc115f6599d5a6e16ea79f98fd9ad03f292d5bb
haoge101 <haoge101@163.com>
2023-07-27 01:00:05 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "A little of every thing is nothing in the main."];   
+const txt  =["HI, THERE!", "To sin by silence when we should protest makes cowards out of men."];   
 		
 var index=0;
 var xiaBiao= 0;
