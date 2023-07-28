5b6913dd87c272bcd5a868de97076bd1b6e4d1c0
haoge101 <haoge101@163.com>
2023-07-28 01:00:45 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "To sin by silence when we should protest makes cowards out of men."];   
+const txt  =["HI, THERE!", "I am still waiting for you in situ, but you have forgotten you had been here once."];   
 		
 var index=0;
 var xiaBiao= 0;
