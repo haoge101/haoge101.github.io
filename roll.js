05ba56ac19a9b21bac004df702b7f163f992e93a
haoge101 <haoge101@163.com>
2023-08-15 00:51:31 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Fear can hold you prisoner,but hope can set you free."];   
+const txt  =["HI, THERE!", "Where there is love,there are always wishes."];   
 		
 var index=0;
 var xiaBiao= 0;
