6839a70a5d5f5daa6be22e654d1dc3f7c71e6204
haoge101 <haoge101@163.com>
2023-08-19 00:49:34 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Victory won't come to me unless I go to it."];   
+const txt  =["HI, THERE!", "You can't blend in when you were born to stand out."];   
 		
 var index=0;
 var xiaBiao= 0;
