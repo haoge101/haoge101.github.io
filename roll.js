28d454445ffad2ad49027875968d65205cadeeda
haoge101 <haoge101@163.com>
2023-08-24 00:51:19 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Shoot for the moon; even if you miss, you'll land among the stars."];   
+const txt  =["HI, THERE!", "The higher I got, the more amazed I was by the view."];   
 		
 var index=0;
 var xiaBiao= 0;
