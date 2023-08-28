e245d0377b362c648b6a6e335ed03d2abc1f1450
haoge101 <haoge101@163.com>
2023-08-28 00:53:02 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Goals determine what you're going to be."];   
+const txt  =["HI, THERE!", "All the bright, precious things fade so fast.And they don't come back."];   
 		
 var index=0;
 var xiaBiao= 0;
