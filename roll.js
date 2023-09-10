54ce0368d067a29f028cab79723561d4cd0c83f2
haoge101 <haoge101@163.com>
2023-09-10 00:55:45 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "I never dreamed about success, I worked for it. "];   
+const txt  =["HI, THERE!", ""The man who can make hard things easy is the educator. ""];   
 		
 var index=0;
 var xiaBiao= 0;
