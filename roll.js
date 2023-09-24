06e32541a264dbd2830c54c1f9eea9abb80a7138
haoge101 <haoge101@163.com>
2023-09-24 00:56:07 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "The road to success and the road to failure are almost exactly the same."];   
+const txt  =["HI, THERE!", "Don't be afraid to give up the good to go for the great. "];   
 		
 var index=0;
 var xiaBiao= 0;
