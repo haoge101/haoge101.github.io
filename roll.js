ca2929930788d330271013620e2e06fee6f8da3f
haoge101 <haoge101@163.com>
2023-09-26 00:54:02 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "I failed my way to success. "];   
+const txt  =["HI, THERE!", "What's dangerous is not to evolve."];   
 		
 var index=0;
 var xiaBiao= 0;
