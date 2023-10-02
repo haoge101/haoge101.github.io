813e9c023c950de9917e4849e68a83668d5f7654
haoge101 <haoge101@163.com>
2023-10-02 00:54:23 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "For thousands of years, mountains and rivers are the answer."];   
+const txt  =["HI, THERE!", " There is nothing permanent except change."];   
 		
 var index=0;
 var xiaBiao= 0;
