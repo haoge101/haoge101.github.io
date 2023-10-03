f3895d20019cb5876143ae98584bde9357e44909
haoge101 <haoge101@163.com>
2023-10-03 00:54:24 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", " There is nothing permanent except change."];   
+const txt  =["HI, THERE!", "The world is before you and you need not take it or leave it as it was when you came in."];   
 		
 var index=0;
 var xiaBiao= 0;
