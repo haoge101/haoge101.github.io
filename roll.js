583006113963e84a90ca5fd0ef1f2527bb40224a
haoge101 <haoge101@163.com>
2023-11-09 00:54:42 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Nothing is impossible; the word itself says 'I'm possible!"];   
+const txt  =["HI, THERE!", "Whatever you are, be a good one."];   
 		
 var index=0;
 var xiaBiao= 0;
