c3093fa4b00d8284cccaef56872c0024243a0936
haoge101 <haoge101@163.com>
2023-11-24 00:55:24 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "All great achievements require time."];   
+const txt  =["HI, THERE!", " I have stood on a mountain of no’s for one yes."];   
 		
 var index=0;
 var xiaBiao= 0;
