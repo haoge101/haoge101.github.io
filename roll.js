ad6d90c93cc48bc875114dea7cae88d691181df6
haoge101 <haoge101@163.com>
2023-12-12 00:57:46 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "If your actions inspire others to dream more, learn more, do more and become more, you are a leader."];   
+const txt  =["HI, THERE!", "Growth and comfort do not coexist."];   
 		
 var index=0;
 var xiaBiao= 0;
