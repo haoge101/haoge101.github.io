5a2156ca4f07aafabd6f830473dc0a7096bfac7c
haoge101 <haoge101@163.com>
2024-02-09 00:53:11 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Years fly by, but the heart stays in the same place."];   
+const txt  =["HI, THERE!", "Celebrate the new year together, welcoming and bidding farewell within one night."];   
 		
 var index=0;
 var xiaBiao= 0;
