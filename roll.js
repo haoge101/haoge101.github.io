9566601ec4c19c67691b6a5812c68b510f034974
haoge101 <haoge101@163.com>
2024-02-14 00:54:31 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Done is better than perfect."];   
+const txt  =["HI, THERE!", "I want love or death."];   
 		
 var index=0;
 var xiaBiao= 0;
