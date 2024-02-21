7bf264e52fc3f20c5785518d8f18a945e7a9bab4
haoge101 <haoge101@163.com>
2024-02-21 00:54:10 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "It's only those who do nothing that make no mistakes."];   
+const txt  =["HI, THERE!", "A goal is a dream with a deadline."];   
 		
 var index=0;
 var xiaBiao= 0;
