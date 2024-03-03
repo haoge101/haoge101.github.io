4490dd81eed789c3da09a2b46e7203f4ee3d5160
haoge101 <haoge101@163.com>
2024-03-03 00:56:23 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "when life gets you down, just keep moving."];   
+const txt  =["HI, THERE!", "Fearless is having fears but jumping anyway."];   
 		
 var index=0;
 var xiaBiao= 0;
