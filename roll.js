3683508f4fa33a549d6692730fd696b77330c9dd
haoge101 <haoge101@163.com>
2024-03-08 00:53:30 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Sometimes things have to fall apart to make way for better things."];   
+const txt  =["HI, THERE!", "You are beautiful just as you are."];   
 		
 var index=0;
 var xiaBiao= 0;
