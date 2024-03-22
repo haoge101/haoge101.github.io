da97dc9fbaf8cc67da83bafad817bf012b84c324
haoge101 <haoge101@163.com>
2024-03-22 00:53:48 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Spring is when life's alive in everything."];   
+const txt  =["HI, THERE!", "Every cloud has a silver lining."];   
 		
 var index=0;
 var xiaBiao= 0;
