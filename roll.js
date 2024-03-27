67529f11dc085578faf1c8c3b0d41fba96a20d7e
haoge101 <haoge101@163.com>
2024-03-27 00:54:07 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Enjoy it while it lasts."];   
+const txt  =["HI, THERE!", "Elegance is the only beauty that never fades."];   
 		
 var index=0;
 var xiaBiao= 0;
