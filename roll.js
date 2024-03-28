d36907d272a14d6eac99e969ce2e04a64fd92c6e
haoge101 <haoge101@163.com>
2024-03-28 00:54:24 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Elegance is the only beauty that never fades."];   
+const txt  =["HI, THERE!", "Travel imparts new vigor to the mind."];   
 		
 var index=0;
 var xiaBiao= 0;
