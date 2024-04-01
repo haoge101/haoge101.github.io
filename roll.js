8a256f822b51f8d822ac9cf8ec8928825d1e5abb
haoge101 <haoge101@163.com>
2024-04-01 00:59:44 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "We will not go quietly into the night!"];   
+const txt  =["HI, THERE!", "It's April Fools' Day. If I believe you, I'm a noodle."];   
 		
 var index=0;
 var xiaBiao= 0;
