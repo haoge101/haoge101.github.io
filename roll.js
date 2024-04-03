306eb3ed2246f011f848d061a9d118cd95ea3143
haoge101 <haoge101@163.com>
2024-04-03 00:54:01 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "A friend is easier lost than found."];   
+const txt  =["HI, THERE!", "Patience is the best remedy."];   
 		
 var index=0;
 var xiaBiao= 0;
