08a743dc76e79ac1c6c331653fe7224afb785807
haoge101 <haoge101@163.com>
2024-04-04 00:55:46 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Patience is the best remedy."];   
+const txt  =["HI, THERE!", "A drizzling rain falls like tears on the Mourning Day."];   
 		
 var index=0;
 var xiaBiao= 0;
