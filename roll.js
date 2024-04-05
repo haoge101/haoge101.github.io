afe5eef28eb9dcea2a85e40a9dd6ec5b29d30d81
haoge101 <haoge101@163.com>
2024-04-05 00:55:00 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "A drizzling rain falls like tears on the Mourning Day."];   
+const txt  =["HI, THERE!", "The best way to achieve a goal is to devote 100% of your time and energy to it."];   
 		
 var index=0;
 var xiaBiao= 0;
