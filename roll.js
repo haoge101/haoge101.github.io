427955c80221e6eb81887cc95435982d46cad68e
haoge101 <haoge101@163.com>
2024-04-25 00:57:58 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Actions speak louder than words."];   
+const txt  =["HI, THERE!", "A friend without faults will never be found."];   
 		
 var index=0;
 var xiaBiao= 0;
