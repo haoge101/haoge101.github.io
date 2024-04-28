a2117f8657188125d76b8bd3b05122b66c27ec7f
haoge101 <haoge101@163.com>
2024-04-28 01:00:26 +0000

    upload img.jpg

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 
 const text = document.querySelector('.texttype');
 
-const txt  =["HI, THERE!", "Children are what the mothers are."];   
+const txt  =["HI, THERE!", "The honest man takes pains, and then enjoys pleasure."];   
 		
 var index=0;
 var xiaBiao= 0;
